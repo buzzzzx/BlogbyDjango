0557ab0edd85893519f52793df25c50f7f400da9
binja <buzzzzx233@gmail.com>
2017-10-13 23:52:00 +0800

    模版标签设计

---
+++ apps/blog/templatetags/blog_tags.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'buzz'
+__date__ = '2017/10/13 下午10:24'
+
+from django import template
+from django.db.models import Count
+
+register = template.Library()
+
+from ..models import Post
+
+
+@register.simple_tag
+def total_posts():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
+
+
+@register.assignment_tag
+def get_most_comments_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments'),
+    ).order_by('-total_comments')[:count]
